cb1b5078f1bb2e04d46c3f64a00938f440a2d80e
The-Night7 <myriamthe7@gmail.com>
2026-05-20 16:05:33 +0200

    python à jour

--- Partie 3/EquaSchro1d2A.py
+++ Partie 3/EquaSchro1d2A.py
@@ -1,4 +1,5 @@
 import numpy as np
+import GaussWP from /Partie 2/PaquetOndeGauss1d2A.py
 
 #3.1.1.b
 def derivee_premiere(y, dx):
@@ -20,3 +21,27 @@ def calculer_derivee_seconde(y, dx):
     d2y[1:-1] = (y[:-2] - 2*y[1:-1] + y[2:])
     dx**2
     return d2y
+
+#3.2.2
+psi = np.zeros((nt, nx), dtype=complex)
+psi[0, :] = GaussWP(k0, a, x, 0)
+
+#3.2.3
+x = np.linspace(xmin, xmax, nx)
+t = np.linspace(tmin, tmax, nt)
+dx = x[1] - x[0]
+dt = t[1] - t[0]
+
+#3.2.4
+# Pré-calcul de la constante pour optimiser l'itération
+coeff = dt / (1j * hbar)
+
+for n in range(0, nt - 1):
+    # Calcul de la dérivée seconde spatiale à l'instant n (points intérieurs)
+    d2psi_dx2 = (psi[:-2, n] - 2*psi[1:-1, n] + psi[2:, n]) / dx**2
+    
+    # Action de l'opérateur Hamiltonien sur la fonction d'onde
+    H_psi = -(hbar**2 / (2*m)) * d2psi_dx2 + V0 * psi[1:-1, n]
+    
+    # Mise à jour de la fonction d'onde à l'instant n+1
+    psi[1:-1, n+1] = psi[1:-1, n] + coeff * H_psi
